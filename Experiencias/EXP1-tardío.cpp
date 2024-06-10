bc01d887939d169058648e33616039b5d325518e
Miguel Angel <138403396+TrojanGh0st@users.noreply.github.com>
2024-06-10 18:52:06 -0500

    EXP1-tardío.cpp

--- Experiencias/EXP1-tardío.cpp
+++ Experiencias/EXP1-tardío.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+class Animal {
+public:
+    virtual void hacerSonido() {
+        cout << "El animal hace un sonido" << endl;
+    }
+};
+
+class Perro : public Animal {
+public:
+    void hacerSonido() override {
+        cout << "El perro ladra" << endl;
+    }
+};
+
+int main() {
+    Animal* miAnimal = new Perro(); // Enlace tardío, variable de tipo Animal pero objeto de tipo Perro
+    miAnimal->hacerSonido(); // Se llama al método hacerSonido() de la clase derivada Perro
+    delete miAnimal;
+    return 0;
+}
